045ab9d8b96ad21bd3ece29d58099ecbf9cb0b12
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2019-11-14 14:49:49 -0600

    cambios DOM

--- src/data.js
+++ src/data.js
@@ -2,6 +2,17 @@
 
 // esta es una función de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
+*/
+
+  export const character = (species) => {
+    species: "alien", 
+    {
+    species: "human",
+    },
+    humans = characters.filter(character =>character.species= "human");
+    return 'character';
+    },
+ 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@
  * import LoL from './data/lol/lol.js'
  * import POTTER from './data/potter/potter.js'
  */
-import  dataSet  from './data/rickandmorty/rickandmorty.js';
+import { character1 } from './data.js';
+ import  dataSet  from './data/rickandmorty/rickandmorty.js';
 
 console.log(dataSet);
+console.log(character1)
 
+document.querySelector('#imgHumano').addEventListener('click',		
+function(e){		
+e.preventDefault();		
+		
+document.querySelector('#root').classList.add('esconder');		
+document.querySelector('#mostrarDataHum').classList.remove('esconder');		
+});
+ 
 /*
  * console.log(POKEMON);
  * console.log(LoL);
