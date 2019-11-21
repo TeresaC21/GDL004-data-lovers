a0d57c32029bcadbb5ae594cc86d06972fcbb335
biancardona <biancardona.rivera@gmail.com>
2019-11-21 09:16:27 -0600

    last changes in DOM

--- src/data.js
+++ src/data.js
@@ -1,8 +1,41 @@
 import dataSet from './data/rickandmorty/rickandmorty.js';
+// filtering species
+const humans = dataSet.results.filter((human) => human.species === 'Human');
+const aliens = dataSet.results.filter((alien) => alien.species === 'Alien');
+const humanoids = dataSet.results.filter((humanoid) => humanoid.species === 'Humanoid');
+const unknowns = dataSet.results.filter((unkno) => unkno.species === 'unknown');
+const poopybuttholes = dataSet.results.filter((poopybutthole) => poopybutthole.species === 'Poopybutthole');
+const mythologs = dataSet.results.filter((mytholog) => mytholog.species === 'Mytholog');
+const animals = dataSet.results.filter((animal) => animal.species === 'Animal');
+const vampires = dataSet.results.filter((vampire) => vampire.species === 'Vampire');
+const robots = dataSet.results.filter((robot) => robot.species === 'Robot');
+const cronenbergs = dataSet.results.filter((cronenberg) => cronenberg.species === 'Cronenberg');
+const diseases = dataSet.results.filter((disease) => disease.species === 'Disease');
+const parasites = dataSet.results.filter((parasite) => parasite.species === 'Parasite');
 
-const resultMap = new Map(Object.entries(dataSet.results));
-console.log(resultMap);
-resultMap.forEach((value, key) => {
-  console.log(key + '=' + value.species);
+/* adding all different species
+const allSpecies = dataSet.results.map((specie) =>
+{ //trae todas las especies de la data
+return specie.species;
 });
+const different = (valor, indice, self) =>
+ {
+  return self.indexOf(valor) === indice;
+}
+const twelveSpecies = allSpecies.filter(different);
+console.log(twelveSpecies, 'twelve Species'), */
 
+export {
+  humans,
+  aliens,
+  humanoids,
+  unknowns,
+  poopybuttholes,
+  mythologs,
+  animals,
+  vampires,
+  robots,
+  cronenbergs,
+  diseases,
+  parasites,
+};
--- src/main.js
+++ src/main.js
@@ -1,15 +1,34 @@
+import dataSet from './data/rickandmorty/rickandmorty.js';
+import {
+  humans,
+  aliens,
+  humanoids,
+  unknowns,
+  poopybuttholes,
+  mythologs,
+  animals,
+  vampires,
+  robots,
+  cronenbergs,
+  diseases,
+  parasites,
+} from './data.js';
 
-
-document.querySelector('#imgHumano').addEventListener('click', 
-function() {
-
-document.querySelector('#imgAlien').classList.add('esconder');
-document.querySelector('#imgRobot').classList.add('esconder');	
-document.querySelector('#imgHumanoide').classList.add('esconder');
-document.querySelector('#imgParasito').classList.add('esconder');
-
-document.querySelector('#resultadosHum').classList.remove('esconder');
-//aqui traeriamos la data.js
-
-export species from './data.js';
-
+const showInfoToHumans = (humans) => {
+  for (let i = 0; i < humans.length; i++) {
+    const allHumans = document.createElement('div');
+    allHumans.className = 'characters';
+    allHumans.innerHTML = `<p>${humans[i].name}</p> ` + `<img src=${humans[i].image}></img>`
+    document.querySelector('#fin').appendChild(allHumans);
+  }
+}
+document.querySelector('#imgHumano').addEventListener('click',
+  () => {
+    document.querySelector('#imgAlien').classList.add('esconder');
+    document.querySelector('#imgRobot').classList.add('esconder');
+    document.querySelector('#imgHumanoide').classList.add('esconder');
+    document.querySelector('#imgParasito').classList.add('esconder');
+    // aqui traeriamos la data.js
+    document.querySelector('#resultadosHum').classList.remove('esconder');
+    document.querySelector('#resultadosHum').textContent = showInfoToHumans (dataSet.results);
+  });
