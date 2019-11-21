1969c36ef5cd931c7ab22b50dac4878670197a41
Teresa Carbajal <Tere@99.200-57-1.bestelclientes.com.mx>
2019-11-21 08:26:21 -0600

    filters and DOM

--- src/data.js
+++ src/data.js
@@ -1,29 +1,38 @@
-/* Manejo de data */
+import dataSet from './data/rickandmorty/rickandmorty.js';
+// filtering species
+const humans = dataSet.results.filter((human) => human.species === 'Human');
+const aliens = dataSet.results.filter((alien) => alien.species === 'Alien');
+const humanoids = dataSet.results.filter((humanoid) => humanoid.species === 'Humanoid');
+const unknowns = dataSet.results.filter((unkno) => unkno.species === 'unknown');
+const poopybuttholes = dataSet.results.filter((poopybutthole) => poopybutthole.species === 'Poopybutthole');
+const mythologs = dataSet.results.filter((mytholog) => mytholog.species === 'Mytholog');
+const animals = dataSet.results.filter((animal) => animal.species === 'Animal');
+const vampires = dataSet.results.filter((vampire) => vampire.species === 'Vampire');
+const robots = dataSet.results.filter((robot) => robot.species === 'Robot');
+const cronenbergs = dataSet.results.filter((cronenberg) => cronenberg.species === 'Cronenberg');
+const diseases = dataSet.results.filter((disease) => disease.species === 'Disease');
+const parasites = dataSet.results.filter((parasite) => parasite.species === 'Parasite');
 
-// esta es una función de ejemplo
+//  adding all of differents species
+const allSpecies = dataSet.results.map((specie) => specie.species);
+console.log(allSpecies);
+const different = (valor, indice, self) => self.indexOf(valor) === indice;
+const twelveSpecies = allSpecies.filter(different);
+console.log(twelveSpecies, 'twelve species');
 
-/*export const example = () => {
-  return 'example';
+
+export {
+  humans,
+  aliens,
+  humanoids,
+  unknowns,
+  poopybuttholes,
+  mythologs,
+  animals,
+  vampires,
+  robots,
+  cronenbergs,
+  diseases,
+  parasites,
+  twelveSpecies
 };
-*/
-import  dataSet  from './data/rickandmorty/rickandmorty.js';
-
-const person = dataSet.results.filter((humano) => {
-   return humano.species === 'Human';
- });
- const nombres =dataSet.results.map((nombre) => {
-   return nombre.name;
- });
-
-export default nombres;
-
-
-  //export const example = (species) => {
-    /*species: "alien", 
-    {
-    species: "human",
-    },
-    humans = characters.filter(character =>character.species= "human");*/
-    //return 'example'; }
-    
- 
--- src/main.js
+++ src/main.js
@@ -1,45 +1,67 @@
+import dataSet from './data/rickandmorty/rickandmorty.js';
+import {
+  humans,
+  aliens,
+  humanoids,
+  unknowns,
+  poopybuttholes,
+  mythologs,
+  animals,
+  vampires,
+  robots,
+  cronenbergs,
+  diseases,
+  parasites,
+  twelveSpecies
+} from './data.js';
 
+const idHuman = document.querySelector('#imgHumano');
+const idAlien = document.querySelector('#imgAlien');
+const idRobot = document.querySelector('#imgRobot');
+const idHumanoide = document.querySelector('#imgHumanoide');
+const idParasite = document.querySelector('#imgParasito');
+/*const idIdunknown = document.querySelector('#img');
+const idPoopybuttbole = document.querySelector('#img');
+const idMytholog = document.querySelector('#img');
+const idAnimal = document.querySelector('#img');
+const idVampire = document.querySelector('#img');
+const idCronenberg = document.querySelector('#img');
+const idDisease = document.querySelector('#img');*/
 
-
- import  person  from './data.js';
-
-
-console.log(person);
-
-
-
-document.querySelector('#imgHumano').addEventListener('click', 
-() => {
-
-document.querySelector('#imgAlien').classList.add('esconder');
-document.querySelector('#imgRobot').classList.add('esconder');	
-document.querySelector('#imgHumanoide').classList.add('esconder');
-document.querySelector('#imgParasito').classList.add('esconder');
-
-//aqui traeriamos la data.js
-document.querySelector('#resultadosHum').classList.remove('esconder');
-document.querySelector('#resultadosHum').textContent =  person;
-
-
-//document.querySelector('#resultadosHum').textContent;		
-});
-
-/*document.querySelector('#imgHumano').addEventListener('click', cargarJS);		
-function cargarJS() {		
-    fetch('rickandmorty.json')
-        .then(function(res){
-            return res.json();
-        })
-        .then(function(data){
-            let html ='';
-            data.forEach(function(human) {
-                html += `
-                <li>${human.specie} ${human.name}</li> 
-                `;
-            })
-            document.querySelector('resultados').innerHTML = html;
-        })
-    }*/
-				
-
- 
+const showInfoToHumans = (showHumans) => {
+  for (let i = 0; i < showHumans.length; i++) {
+    const allHumans = document.createElement('div');
+    allHumans.className = 'characters';
+    allHumans.innerHTML = `<p>${showHumans[i].name}</p> ` + `<img src=${showHumans[i].image}></img>` + `<p>${showHumans[i].species}</p>`
+    document.querySelector('#fin').appendChild(allHumans);
+  }
+}
+idHuman.addEventListener('click',
+  () => {
+    idAlien.classList.add('esconder');
+    idRobot.classList.add('esconder');
+    idHumanoide.classList.add('esconder');
+    idParasite.classList.add('esconder');
+    // aqui traemos la data de humanos
+    document.querySelector('#resultados').classList.remove('esconder');
+    document.querySelector('#resultados').textContent = showInfoToHumans(humans);
+  });
+  const showInfoToAliens = (showAliens) => {
+    for (let i = 0; i < showAliens.length; i++) {
+      const allAliens = document.createElement('div');
+      allAliens.className = 'characters';
+      allAliens.innerHTML = `<p>${showAliens[i].name}</p> ` + `<img src=${showAliens[i].image}></img>` + `<p>${showAliens[i].species}</p>`
+      document.querySelector('#fin').appendChild(allAliens);
+    }
+  }
+  idAlien.addEventListener('click',
+    () => {
+      idHuman.classList.add('esconder');
+      idRobot.classList.add('esconder');
+      idHumanoide.classList.add('esconder');
+      idParasite.classList.add('esconder');
+      // aqui traemos la data de humanos
+      document.querySelector('#resultados').classList.remove('esconder');
+      document.querySelector('#resultados').textContent = showInfoToAliens(aliens);
+    });
+  
